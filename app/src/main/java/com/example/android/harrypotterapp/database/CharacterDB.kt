b3f20365e605f7dc2b1de925e028bdddf3b74cb3
jrenzzo <u202118568@upc.edu.pe>
2022-10-04 14:45:31 -0500

    chore: created fragment xml files for character and favourite character

--- app/src/main/java/com/example/android/harrypotterapp/database/CharacterDB.kt
+++ app/src/main/java/com/example/android/harrypotterapp/database/CharacterDB.kt
@@ -1,12 +1,23 @@
 package com.example.android.harrypotterapp.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Character::class], version = 1)
 abstract class CharacterDB: RoomDatabase() {
     abstract fun getCharacterDao(): CharacterDao
     companion object {
-
+        private var INSTANCE: CharacterDB?=null
+        fun getInstance(context: Context):CharacterDB{
+            if(INSTANCE==null){
+                INSTANCE= Room
+                    .databaseBuilder(context, CharacterDB::class.java, "character.db")
+                    .allowMainThreadQueries()
+                    .build()
+            }
+            return INSTANCE as CharacterDB
+        }
     }
 }
